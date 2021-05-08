57055d36557935ad3679d938d4f95a10493259e8
Olle Welin <olle_welin@hotmail.com>
2021-05-08 17:42:23 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -19,12 +19,118 @@
 
 
 using namespace std;
-const int input_img_size = 224;
+using namespace cv;
+
+Mat transf(Mat src)
+{
+   Point2f srcTri[3];
+   Point2f dstTri[3];
+
+    float scale_r;
+    float scale_r_n;
+    float scale_gain=0.15f;///Change here
+    float trans_x_r;
+    float trans_y_r;
+    float transelation_x = 0.0f;
+    float transelation_y = 0.0f;
+    float x_gain = 0.0f;///Change here
+    float y_gain = 0.0f;///Change here
+    float rot_max_min = 25.0f;///Change here
+    float rot_random =0.0f;
+
+ //   srand (static_cast <unsigned> (time(0)));//Seed the randomizer
+    scale_r = (float) (rand() % 65535) / 65536;
+ //   printf("scale_r %f\n", scale_r);
+    trans_x_r = (float) (rand() % 65535) / 65536;
+ //   printf("trans_x_r %f\n", trans_x_r);
+    trans_y_r = (float) (rand() % 65535) / 65536;
+ //   printf("trans_y_r %f\n", trans_y_r);
+    rot_random = (float) (rand() % 65535) / 65536;
+ //   printf("rot_random %f\n", rot_random);
+
+    scale_r = 0.5f + scale_r;
+//    printf("scale_r %f\n", scale_r);
+    scale_r_n = 1.0f - scale_r;
+
+
+transelation_x = (trans_x_r - 0.5f) * x_gain;
+transelation_y = (trans_y_r - 0.5f) * y_gain;
+
+    scale_r = ((scale_r - 1.0f) * scale_gain) + 1.0f;
+    scale_r_n = scale_r_n * scale_gain;
+//    printf("scale_r_n %f\n", scale_r_n);
+
+   Mat rot_mat( 2, 3, CV_8UC1 );
+   Mat warp_mat( 2, 3, CV_8UC1 );
+   Mat warp_dst, warp_rotate_dst;
+   /// Set the dst image the same type and size as src
+   warp_dst = Mat::zeros( src.rows, src.cols, src.type() );
+   /// Set your 3 points to calculate the  Affine Transform
+   srcTri[0] = Point2f( 0,0 );
+   srcTri[1] = Point2f( src.cols - 1, 0 );
+   srcTri[2] = Point2f( 0, src.rows - 1 );
+
+//2:1
+   dstTri[0] = Point2f( src.cols*0.25, src.rows*0.25 );
+   dstTri[1] = Point2f( src.cols*0.75, src.rows*0.25 );
+   dstTri[2] = Point2f( src.cols*0.25, src.rows*0.75 );
+
+
+   /// Get the Affine Transform
+   warp_mat = getAffineTransform( srcTri, dstTri );
+
+   /// Apply the Affine Transform just found to the src image
+   warpAffine( src, warp_dst, warp_mat, warp_dst.size() );
+
+
+//   dstTri[0] = Point2f( src.cols*(0.25+transelation_x), src.rows*0.25 );
+//   dstTri[1] = Point2f( src.cols*(0.75+transelation_x), src.rows*0.25 );
+//   dstTri[2] = Point2f( src.cols*(0.25+transelation_x), src.rows*0.75 );
+
+//   dstTri[0] = Point2f( src.cols*(0.0+transelation_x), src.rows*(0.0+transelation_y) );
+//   dstTri[1] = Point2f( src.cols*(1.0+transelation_x), src.rows*(0.0+transelation_y) );
+//   dstTri[2] = Point2f( src.cols*(0.0+transelation_x), src.rows*(1.0+transelation_y) );
+
+   dstTri[0] = Point2f( src.cols*(scale_r_n+transelation_x), src.rows*(scale_r_n+transelation_y) );
+   dstTri[1] = Point2f( src.cols*(scale_r+transelation_x), src.rows*(scale_r_n+transelation_y) );
+   dstTri[2] = Point2f( src.cols*(scale_r_n+transelation_x), src.rows*(scale_r+transelation_y) );
+
+    /// Get the Affine Transform
+   warp_mat = getAffineTransform( srcTri, dstTri );
+
+   /// Do the Affine Transformation scale and translation
+   warpAffine( src, warp_dst, warp_mat, warp_dst.size() );
+
+//================== Rotation ==============================
+   /** Rotating the image after Warp */
+    rot_random = rot_random - 0.5f;// Make it to a random value of +/- 0.5
+    rot_random = rot_random * rot_max_min;
+//    printf("rot_random %f\n", rot_random);
+
+   /// Compute a rotation matrix with respect to the center of the image
+   Point center = Point( warp_dst.cols/2, warp_dst.rows/2 );
+   double angle = 0.0;
+   double rot_scale = 1.0;
+    angle = (double)rot_random;
+   /// Get the rotation matrix with the specifications above
+   rot_mat = getRotationMatrix2D( center, angle, rot_scale );
+
+   /// Rotate the warped image
+   warpAffine( warp_dst, warp_rotate_dst, rot_mat, warp_dst.size() );
+//============= End Rotation ==============================
+//   imshow("warp_dst", warp_dst);
+//   imshow("warp_rotate_dst", warp_rotate_dst);
+//   imshow("src", src);
+
+    return warp_rotate_dst;
+}
+
 cv::Mat makeSquareImg(cv::Mat frame, int outputImageSize, std::string file_location)
 {
   
   cv::Mat rectImage, graymat;
   frame.convertTo(frame, CV_32FC3, 1.0f / 255.0f);
+  frame = transf(frame);
   //Resize input image to fit into a example 224 x 224 dataset images
   //First step scale the input image so the least side h or w will be resized to example 224
   //Next step crop the larger h or w to example 224 pixel as well, crop with images in center
@@ -36,7 +142,7 @@ cv::Mat makeSquareImg(cv::Mat frame, int outputImageSize, std::string file_locat
   cv::Mat varLight;
 //  for(int test=0;test<10;test++){
 
-  
+  printf("debugX \n");
   float Rando = (float) (rand() % 65535) / 65536;//0..1.0 range
   if (frame.rows == 0 || frame.cols == 0)
   {
@@ -49,60 +155,52 @@ cv::Mat makeSquareImg(cv::Mat frame, int outputImageSize, std::string file_locat
   }
   else
   {
-    
-    if (frame.rows > frame.cols)
-    {
-      //Input images is a portrait mode
-      //Calculate the starting point of the square rectangle
-      inputSize = frame.cols;
-  //    y_start = (frame.rows / 2) - inputSize / 2;
-      float y_start_flt = ((float)(frame.rows - inputSize) * Rando);
-      y_start = (int)y_start_flt;
-      if(y_start < 0){
-        y_start = 0;
-      }
-      if(y_start > frame.rows-1){
-        y_start = frame.rows-1;
-      }
-      x_start = 0;
-    }
-    else
-    {
-      //Input images is a landscape mode
-      //Make a square rectangle of input image
-      inputSize = frame.rows;
-    //  x_start = (frame.cols / 2) - inputSize / 2;
-      float x_start_flt = ((float)(frame.cols - inputSize) * Rando);
-      x_start = (int)x_start_flt;
-      if(x_start < 0){
-        x_start = 0;
-      }
-      if(x_start > frame.cols-1){
-        x_start = frame.cols-1;
-      }
-      y_start = 0;
-    }
-    //Make a square rectangle of input image
-    //Mat rect_part(image, Rect(rand_x_start, rand_y_start, Width, Height));//Pick a small part of image
-    cv::Mat rectImageTemp(frame, cv::Rect(x_start, y_start, inputSize, inputSize)); //
-    //Size size(input_image_width,input_image_height);//the dst image size,e.g.100x100
-    cv::Size outRectSize(outputImageSize, outputImageSize);
-    //resize(src,dst,size);//resize image
-    cv::resize(rectImageTemp, rectImage, outRectSize);
   }
+/// first center crop image to cut away rotation black corners
 
-  //Flip
-//  Rando = (float) (rand() % 65535) / 65536;//0..1.0 range
-//  cv::Mat unfliped = rectImage.clone();
-//  if (Rando > 0.5){
-//    cv::flip(rectImage,rectImage,1);//1 x axis flip
-//  }
+    //Size up image 
+    const float size_up = 1.55f;
+    //Size size(input_image_width,input_image_height);//the dst image size,e.g.100x100
+    int frm_col = frame.cols;
+    int frm_row = frame.rows;
+    int x_size =  (int)(size_up*(float)frame.cols);
+//    printf("x_size = %d\n", x_size);
+    int y_size = (int)(size_up*(float)frame.rows);
+//    printf("y_size = %d\n", y_size);
+//    printf("frame.cols =%d\n", frame.cols);
+//    printf("frame.rows =%d\n", frame.rows);
+
+    cv::Size outRectSize(x_size, y_size);
+    
+    //resize(src,dst,size);//resize image
+    cv::resize(frame,frame,outRectSize);
+//    printf("frame.cols =%d\n", frame.cols);
+//    printf("frame.rows =%d\n", frame.rows);
+
+   // cv::imshow("frame", frame);
+   // cv::waitKey(5000);
+    //Mat rect_part(image, Rect(rand_x_start, rand_y_start, Width, Height));//Pick a small part of image
+    x_start = (frame.cols-frm_col)/2;
+    y_start = (frame.rows-frm_row)/2;
+ //   printf("x_start =%d\n", x_start);
+ //   printf("y_start =%d\n", y_start);
+    int width = frm_col;
+    int height = frm_row;
+ //   printf("width =%d\n", width);
+ //   printf("height =%d\n", height);
+    //Mat rect_part(image, Rect(rand_x_start, rand_y_start, Width, Height));//Pick a small part of image
+    cv::Mat rectImageTemp(frame, cv::Rect(x_start, y_start, width, height)); //
+    //Size size(input_image_width,input_image_height);//the dst image size,e.g.100x100
+//    cv::imshow("rectImageTemp", rectImageTemp);
+//    cv::waitKey(5000);
+///
+
   Rando = (float) (rand() % 65535) / 65536;//0..1.0 range
-  float light_rand = 1.0f;
-  Rando *= light_rand;
-  Rando -= light_rand / 2.0f;
+  const float light_rand_strengt = 0.5f;
+  Rando *= light_rand_strengt;
+  Rando -= light_rand_strengt / 2.0f;
 
-  varLight = rectImage.clone();
+  varLight = rectImageTemp.clone();
   varLight = varLight * (1.0f + Rando);
 /*
   cv::imshow("frame", frame);
@@ -112,6 +210,11 @@ cv::Mat makeSquareImg(cv::Mat frame, int outputImageSize, std::string file_locat
   cv::waitKey(5000);
 */
 //  }
+  varLight.convertTo(varLight, CV_8UC3, 255);
+ //  cv::imshow("varLight", varLight);
+ //   cv::waitKey(5000);
+
+
   return varLight;
 }
 
@@ -128,8 +231,7 @@ auto main() -> int
   srand (static_cast <unsigned> (time(0)));///Seed the randomizer (need to do only once)
   srand(time(NULL));
 
-  int test_class = 1;
-  int train_class = 1;
+ 
   int rand_img_indx;
   std::string str1 = "./data/";
   std::string str2 = "/";
@@ -159,41 +261,49 @@ auto main() -> int
 
 
     rand_img_indx = rand() % (1000);
-    std::string train_class_str = std::to_string(train_class);
-    std::string train_img_str = std::to_string(rand_img_indx);
-    train_file = str1 + train_class_str + str2 + train_img_str + str3;
     std::string input_file;
     printf("\n");
     printf("************ Test image ************* \n");
 
 
   //  rand_img_indx = rand() % (custom_dataset_size / nr_of_classes);
-    std::string test_class_str = std::to_string(test_class);
     std::string inp_img_str = std::to_string(rand_img_indx);
-    std::string flip_img_str = std::to_string(rand_img_indx);
-    input_file = str1 + test_class_str + str2 + inp_img_str + str3;
-    input_file = str1 + test_class_str + str2 + "617" + str3;
-
+    std::string output_file = std::to_string(rand_img_indx);
+  const int nr_randomize_images = 5;
+  printf("Randomize input images by factor =%d\n", nr_randomize_images);
   for(int i=0;i<nr_of_files;i++)
   {
     inp_img_str = std::to_string(i+1);
     input_file = str1 + inp_img_str + str3;
-    inp_img_str = std::to_string(i+nr_of_files+1);
-    flip_img_str = str1 + inp_img_str + str3;
     printf("input_file = ");
     cout << input_file << endl;
-
     cv::Mat img = cv::imread(input_file);
-    cv::Mat fliped_img;
+    cv::imshow("input img", img);
+    for(int r=0;r<nr_randomize_images;r++)
+    {
+      output_file = std::to_string(r+50000 + nr_randomize_images*i);
+      output_file = str1 + output_file + str3;
+      int random_sized_img = rand() % 350;
+      random_sized_img += 224;//Minimum 224 pixel
+      printf("Random size =%d\n", random_sized_img);
+      cv::Mat imgSquarRect = makeSquareImg(img, random_sized_img, input_file);
+      printf("output_file = ");
+      cout << output_file << endl;
+      
+      //cv::imwrite("output file images", imgSquarRect);
+      cv::imshow("output file images", imgSquarRect);
+      cv::waitKey(1000);
+      cv::imwrite(output_file, imgSquarRect);
+    }
+
+
+  //  cv::Mat img = cv::imread(input_file);
+ //   cv::Mat fliped_img;
    // cv::Mat imgSquarRect = makeSquareImg(img, input_img_size, input_file);
    //cv::flip(src,dst,1);//1 x axis flip
-    cv::flip(img,fliped_img,1);//1 x axis flip
-    cv::imshow("img", img);
-    cv::imshow("fliped_img", fliped_img);
-    printf("output_file = ");
-    cout << flip_img_str << endl;
-    cv::imwrite(flip_img_str, fliped_img);
-    cv::waitKey(100);
+   // cv::flip(img,fliped_img,1);//1 x axis flip
+   // cv::imshow("img", img);
+   // cv::imshow("fliped_img", fliped_img);
 
   }
 }
